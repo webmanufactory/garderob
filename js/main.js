e1674d464dc3bb56c2ccd52acf3aacd7fd37df21
webmanufactory <bubunya@yandex.ru>
2021-02-05 18:22:57 +0300

    new changes. Mobile version

--- js/main.js
+++ js/main.js
@@ -11,10 +11,8 @@ $(document).ready(function() {
 		  ]
 	});
 	$('#callback').on('show.bs.modal', function (event) {
-		var button = $(event.relatedTarget) // Button that triggered the modal
-		var recipient = button.data('title') // Extract info from data-* attributes
-		// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-		// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+		var button = $(event.relatedTarget) 
+		var recipient = button.data('title') 
 		var modal = $(this)
 		modal.find('.title__change').text(recipient)
 	});
